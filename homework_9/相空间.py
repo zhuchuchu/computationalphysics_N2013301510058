f4120ef4725011928684b69d00f9bb6d75750ca4
2013301510058 <1175225179@qq.com>
2016-06-17 16:56:59 +0800

    Update 相空间.py

--- homework_9/相空间.py
+++ homework_9/相空间.py
@@ -33,23 +33,42 @@ class CROMER2(CROMER1):
     def plot_theta2(self,_ax):
         _ax.plot(self.theta, self.omg, '--',label=r'$F_D=1.2$')
        
+class CROMER3(CROMER1):
+    def calculate(self):
+        for i in range(self.n):
+            self.t.append(self.t[-1]+self.dt)
+	    self.omg.append(self.omg[-1]+(-self.g/self.l*np.sin(self.theta[-1])-self.q[1]*self.omg[-1]+self.F_D[0]*np.sin(2./3.*self.t[-1]))*self.dt)
+            self.theta.append(self.theta[-1]+self.omg[-1]*self.dt)
+            if self.theta[i+1]<-math.pi:
+               self.theta[i+1]=self.theta[i+1]+2*math.pi
+            if self.theta[i+1]>math.pi:
+               self.theta[i+1]=self.theta[i+1]-2*math.pi
+    def plot_theta3(self,_ax):
+        _ax.plot(self.theta, self.omg, '--',label=r'$F_D=0$')
         
 fig= plt.figure(figsize=(15,7))
-ax1 = plt.subplot(121)
-ax2 = plt.subplot(122)
-
+ax1 = plt.subplot(132)
+ax2 = plt.subplot(133)
+ax3 = plt.subplot(131)
 comp= CROMER1()
 comp.calculate()
 comp.plot_theta1(ax1)
 comp= CROMER2()
 comp.calculate()
 comp.plot_theta2(ax2)
+comp= CROMER3()
+comp.calculate()
+comp.plot_theta3(ax3)
 ax1.set_title(r'$\omega$ versus $\theta$',fontsize=14)
 ax2.set_title(r'$\omega$ versus $\theta$',fontsize=14)
+ax3.set_title(r'$\omega$ versus $\theta$',fontsize=14)
 ax1.set_xlabel('time(s)',fontsize=14)
 ax1.set_ylabel(r'$\theta$ (radians)',fontsize=14)
 ax2.set_xlabel('time(s)',fontsize=14)
 ax2.set_ylabel(r'$\omega$(radians/s)',fontsize=14)
+ax3.set_xlabel('time(s)',fontsize=14)
+ax3.set_ylabel(r'$\omega$(radians/s)',fontsize=14)
 ax1.legend(fontsize=12,loc='best')
 ax2.legend(fontsize=12,loc='best')
+ax3.legend(fontsize=12,loc='best')
 plt.show(fig)
